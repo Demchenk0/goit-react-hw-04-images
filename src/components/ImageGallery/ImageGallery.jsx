261f80a490de212433463776c307e791740e65fe
Demchenk0 <santadyr2016@gmail.com>
2022-10-07 17:10:10 +0300

    search

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -14,51 +14,51 @@ export function ImageGallery({ searchQuery, page, onChangePage }) {
 	const [modalImage, setmModalImage] = useState(null);
 
 	useEffect(() => {
-		setSpinner(true);
-		fetch(
-			`https://pixabay.com/api/?key=29624202-0ace9f1cfbb26d74e2bd1c2da&q=${searchQuery}&page=${page}&per_page=12`
-		)
-			.then(res => {
-				if (res.ok) {
-					return res.json();
-				}
-			})
-			// !!!!!! Сохроняется результат нашего запроса
-			.then(image => {
-				if (searchQuery === '') {
-					return;
-				}
-				page > 1
-					? setImage(prevImage => [...prevImage, ...image.hits])
-					: setImage(image.hits);
-				if (image.hits.length) {
-					toast.success(`Hooray! We found ${image.total} images.`, {
-						position: 'top-right',
-						autoClose: 1000,
-						hideProgressBar: false,
-						closeOnClick: true,
-						pauseOnHover: true,
-						draggable: true,
-						progress: undefined,
-					});
-				} else {
-					toast.info(
-						`Sorry, there are no ${searchQuery} images matching your search query. Please try again.`,
-						{
+		if (searchQuery) {
+			setSpinner(true);
+			fetch(
+				`https://pixabay.com/api/?key=29624202-0ace9f1cfbb26d74e2bd1c2da&q=${searchQuery}&page=${page}&per_page=12`
+			)
+				.then(res => {
+					if (res.ok) {
+						return res.json();
+					}
+				})
+
+				// !!!!!! Сохроняется результат нашего запроса
+				.then(image => {
+					page > 1
+						? setImage(prevImage => [...prevImage, ...image.hits])
+						: setImage(image.hits);
+					if (image.hits.length) {
+						toast.success(`Hooray! We found ${image.total} images.`, {
 							position: 'top-right',
-							autoClose: 3000,
+							autoClose: 1000,
 							hideProgressBar: false,
 							closeOnClick: true,
 							pauseOnHover: true,
 							draggable: true,
 							progress: undefined,
-						}
-					);
-				}
-			})
-			.catch()
+						});
+					} else {
+						toast.info(
+							`Sorry, there are no ${searchQuery} images matching your search query. Please try again.`,
+							{
+								position: 'top-right',
+								autoClose: 3000,
+								hideProgressBar: false,
+								closeOnClick: true,
+								pauseOnHover: true,
+								draggable: true,
+								progress: undefined,
+							}
+						);
+					}
+				})
+				.catch()
 
-			.finally(() => setSpinner(false));
+				.finally(() => setSpinner(false));
+		}
 	}, [page, searchQuery]);
 
 	const toggleModal = modalImage => {
